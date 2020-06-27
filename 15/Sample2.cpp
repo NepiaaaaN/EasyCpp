942061213020958d92ce2ab0934b437e11198e26
NepiaaaaN <surfase095@gmail.com>
2020-06-27 12:00:20 +0900

    Add Sample2

---
+++ 15/Sample2.cpp
@@ -0,0 +1,8 @@
+#include "Sample2.h"
+
+int main(){
+    Car car1("mycar", 1234, 25.5);
+    car1.show();
+
+    return 0;
+}
--- 15/Sample2.h
+++ 15/Sample2.h
@@ -5,13 +5,34 @@ using namespace std;
 //Car Classの宣言
 class Car{
     private:
-        int num;
-        double gas;
-        char* pName;
+        int num; //車のナンバー
+        double gas; //ガソリン量
+        char* pName; //名前(複数の文字を入力するためCharのPointer型で宣言)
     public:
-        Car(const char* pN, int n, double g);
-        ~Car();
-        void show();
+        Car(const char* pN, int n, double g); //Constructor
+        ~Car(); //Destructor
+        void show(); //表示用メソッド
+};
+
+//Car Classメンバ関数の宣言
+//Constructor
+Car::Car(const char* pN, int n, double g){
+    pName = new char[strlen(pN) + 1]; //実引数のコピーpNとなる文字＋NULL文字分を追加してメモリを動的に確保
+    strcpy(pName, pN); //pNameにpNの内容をコピー
+    num = n;
+    gas = g;
+    cout << pName << "を作成しました。\n" << endl;
 }
 
-//続きから書く
+//Destructor
+Car::~Car(){
+    cout << pName << "を破棄します\n" << endl;
+    delete[] pName; //確保したメモリの破棄
+}
+
+//メンバ関数
+void Car::show(){
+    cout << "車のナンバーは" << num << "です。" << endl;
+    cout << "ガソリン量は" << gas << "です。" << endl;
+    cout << "名前は" << pName << "です。\n" << endl;
+}
